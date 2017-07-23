0d27519e0d2fa819788ae3d6b9fe6c791570dace
Hyunkyu Michael Lee <hyunkyu@The-Computer.local>
2017-07-23 14:34:57 -0700

    Reads png files from dataset and converts to grayscale

--- src/main.py
+++ src/main.py
@@ -7,7 +7,20 @@ def detectFeatures(img):
     return kp
 
 def main():
-    img = cv2.imread('../KITTIDataset/dataset/sequences/00/image_0/000000.png');
+    img0 = None; img1 = None;
+    base = '../KITTIDataset/dataset/sequences/00/images_0/{:06d}.png'
+
+    # Get the first two file names
+    f1 = base.format(0)
+    f2 = base.format(0)
+
+    # Read Images
+    img0 = cv2.imread(f1)
+    img1 = cv2.imread(f2)
+
+    # Convert images to grayscale
+    img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
     features = detectFeatures(img)
     img_with_features = cv2.drawKeypoints(img, features, color=(255, 0, 0), outImage=None)
