d9b733d2bcc95ee03cb75e5ddd7ae3d65892f5d6
Hyunkyu Michael Lee <hyunkyu@The-Computer.local>
2017-07-24 11:12:58 -0700

    Creates class to wrap the previous mono-VO fns
    This will allow for ease of testing as well as clean up some of the code

---
+++ src/MonoVisualOdometer.py
@@ -0,0 +1,6 @@
+class MonoVisualOdometer:
+    def __init__(self, images_file_path, ground_truth_file_path):
+        self.images_file_path       = images_file_path
+        self.ground_truth_file_path = ground_truth_file_path
+
+    
