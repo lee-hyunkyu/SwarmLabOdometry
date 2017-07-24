d6cc960caba68620e00d226a35cda49a99bd582f
Hyunkyu Michael Lee <hyunkyu@The-Computer.local>
2017-07-24 11:50:42 -0700

    Reads the camera parameters from calib file

--- src/MonoVisualOdometer.py
+++ src/MonoVisualOdometer.py
@@ -5,18 +5,30 @@ import re
 import pdb
 
 class MonoVisualOdometer:
-    def __init__(self, images_file_path, ground_truth_file_path):
+    def __init__(self, images_file_path, ground_truth_file_path, calib_file_path):
+        ''' Assumes KITTI Dataset '''
         self.images_file_path       = images_file_path
         self.ground_truth_file_path = ground_truth_file_path
 
-        self.images_file_path       = self.images_file_path + "/{06d}.png"
-        self.ground_truth_file      = open(self.ground_truth_file_path)
+        last_char_in_path = self.images_file_path[-1]
+        if last_char_in_path == '/':
+            self.images_file_path   = self.images_file_path + "{06d}.png"
+        else:
+            self.images_file_path   = self.images_file_path + "/{06d}.png"
+        #self.ground_truth_file      = open(self.ground_truth_file_path)
 
+        # Get Camera Caliberation settings
+        calib_file              = open(calib_file_path)
+        line                    = calib_file.readline()
+        words                   = re.split(' ', line)
+        self.focal              = float(words[1])
+        self.principal_points   = (float(words[3]), float(words[7]))
+
+        # Set the needed variables to None or create if possible
         self.prev_img               = None;
         self.curr_img               = None;
         self.prev_feature_pts       = None;
         self.curr_feature_pts       = None;
-
         self.fast_detector          = cv2.FastFeatureDetector_create()
 
     def end(self):
@@ -62,7 +74,10 @@ class MonoVisualOdometer:
 
         return (img0_features, img1_features)
 
+    def run(self):
+        pass
 
+    
 
 
 
