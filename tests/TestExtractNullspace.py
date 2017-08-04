e16f02dcff8eedf5614cb616a0feacbf3942f38b
Hyunkyu Michael Lee <hyunkyu@The-Computer.local>
2017-08-04 14:21:04 -0700

    Create test to test dot product implementation

--- tests/TestExtractNullspace.py
+++ tests/TestExtractNullspace.py
@@ -5,11 +5,13 @@ from src.util.extract_nullspace import *
 import unittest 
 import math;
 from src import *
+import pickle;
+import numpy.testing as nptest
 
 class TestExtractNullspace(unittest.TestCase):
 
+    @unittest.skip
     def test_cross_product(self):
-        return True; # This has already been tested
         for u1, v1 in zip(range(-100, 100), reversed(range(-100, 100))):
             for u2, v2 in zip(range(-100, 100), reversed(range(-100, 100))):
                 for u3, v3 in zip(range(-100, 100), reversed(range(-100, 100))):
@@ -17,10 +19,8 @@ class TestExtractNullspace(unittest.TestCase):
                     v = [v1, v2, v3];
                     c1 = np.cross(np.array(u), np.array(v));
                     c2 = cross_product(u, v);
-                    for c_1, c_2 in zip(c1, c2):
-                        self.assertEqual(c_1, c_2, 
-                            [u, v])
-
+                    
+    @unittest.skip
     def test_l2_norm(self):
         margin = 1e-10;
         for u1 in range(-100, 100):
@@ -29,11 +29,24 @@ class TestExtractNullspace(unittest.TestCase):
                     u = [u1, u2, u3]
                     np_length = np.linalg.norm(np.array(u))
                     l = length(u)
-                    math.isclose(float(np_length), float(l), rel_tol=margin)
+                    self.assertTrue(math.isclose(float(np_length), float(l), rel_tol=margin))
 
     def test_svd_of_essential_matrix(self):
         pass
 
+    def test_matrix_multiplication(self):
+        pass
+
+    def test_dot_product(self):
+        margin = 1e-10;
+        for u1, v1 in zip(range(-30, 30), reversed(range(-30, 30))):
+            for u2, v2 in zip(range(-30, 30), reversed(range(-30, 30))):
+                for u3, v3 in zip(range(-30, 30), reversed(range(-30, 30))):
+                    u = [u1, u2, u3];
+                    v = [v1, v2, v3];
+                    d1 = np.dot(np.array(u), np.array(v))
+                    d2 = dot_v(u, v);
+                    self.assertEqual(d1, d2)
 
 if __name__ == '__main__':
     unittest.main()
