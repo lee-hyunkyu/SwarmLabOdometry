08e2b559a724d795e5be697b708b9af985d48b42
Hyunkyu Michael Lee <hyunkyu@The-Computer.local>
2017-08-04 13:28:44 -0700

    Adds a file to do unittest

---
+++ tests/TestExtractNullspace.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.insert(0, '..')
+import numpy as np 
+from src.util import *
+import unittest 
+import math;
+from src import *
+
+class TestExtractNullspace(unittest.TestCase):
+
+    def test_cross_product(self):
+        return True; # This has already been tested
+        for u1, v1 in zip(range(-100, 100), reversed(range(-100, 100))):
+            for u2, v2 in zip(range(-100, 100), reversed(range(-100, 100))):
+                for u3, v3 in zip(range(-100, 100), reversed(range(-100, 100))):
+                    u = [u1, u2, u3];
+                    v = [v1, v2, v3];
+                    c1 = np.cross(np.array(u), np.array(v));
+                    c2 = cross_product(u, v);
+                    for c_1, c_2 in zip(c1, c2):
+                        self.assertEqual(c_1, c_2, 
+                            [u, v])
+
+    def test_l2_norm(self):
+        margin = 1e-10;
+        for u1 in range(-100, 100):
+            for u2 in range(-100, 100):
+                for u3 in range(-100, 100):
+                    u = [u1, u2, u3]
+                    np_length = np.linalg.norm(np.array(u))
+                    length = length(U)
+                    math.isclose(np_length, length, rel_tol=margin)
+
+    def test_svd_of_essential_matrix(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
